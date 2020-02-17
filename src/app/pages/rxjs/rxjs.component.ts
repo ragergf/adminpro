7d49c3dfcbe228fad7d7572394984c1113eb494d
Abraham Rager Gomez Flores <ag261y@att.com>
2020-02-16 19:42:44 -0600

    seccion 8 Terminada

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+
+  constructor() {
+    this.subscription = this.regresaObservable()
+    // .pipe(
+    //   retry(2)
+    // )
+    .subscribe(
+      numero => console.log('subs: ',  numero),
+      error => console.error('Error en el obs.', error),
+      () => console.log('El observador termino.')
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('Esta pagina se va cerrar!');
+    this.subscription.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any> {
+    return new Observable( ( observer: Subscriber<any> ) => {
+      let contador = 0;
+      let intervalo = setInterval( () => {
+        contador += 1;
+
+        const salida = {
+          valor: contador
+        };
+
+        observer.next(salida);
+        // if ( contador === 3) {
+        //   clearInterval( intervalo );
+        //   observer.complete();
+        // }
+
+        // if ( contador === 2 ) {
+        //   // clearInterval( intervalo );
+        //   observer.error('Auxilio');
+        // }
+      }, 1000 );
+    }).pipe(
+      map( resp => resp.valor),
+      filter( (valor, index) => {
+        // console.log("filter", valor, index);
+        if ( (valor % 2) === 1 ){
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
+
+}
